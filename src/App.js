58a26682f76d0056c95e37f7e2ff920638864ce2
Olga <888yalosinger@ukr.net>
2023-11-01 20:10:45 -0400

    created a skeleton for the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+const TODO_LIST_MOCKS = [
+  {
+    id: 1,
+    title: "Walk the dog",
+  },
+  {
+    id: 2,
+    title: "Do the dishes",
+  },
+];
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [items, setItems] = useState(TODO_LIST_MOCKS);
+
+  const newItemTitleChange = ({ target: { value } }) => {
+    setTitle(value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newItem = { id: uuid(), title };
+
+    setItems([...items, newItem]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input value={title} onChange={newItemTitleChange} />
+        <button type="submit" onClick={submitHandler}>
+          Add
+        </button>
+      </form>
+
+      <ul>
+        {items.map((item) => {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
