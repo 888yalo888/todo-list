ff4e87b0893e961a88eb638b917f84737e297253
Olga <888yalosinger@ukr.net>
2023-11-08 12:09:45 -0500

    Added button and request that deletes the task from the server

--- src/App.js
+++ src/App.js
@@ -1,49 +1,100 @@
-import "./App.css";
-import { useState } from "react";
-import { v4 as uuid } from "uuid";
-
-const TODO_LIST_MOCKS = [
-  {
-    id: 1,
-    title: "Walk the dog",
-  },
-  {
-    id: 2,
-    title: "Do the dishes",
-  },
-];
+import './App.css';
+import { useCallback, useEffect, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 function App() {
-  const [title, setTitle] = useState("");
-  const [items, setItems] = useState(TODO_LIST_MOCKS);
+    const [title, setTitle] = useState('');
+    const [items, setItems] = useState([]);
 
-  const newItemTitleChange = ({ target: { value } }) => {
-    setTitle(value);
-  };
+    const getData = useCallback(async () => {
+        const response = await fetch(
+            'http://localhost:8001/api/todolist/get-all-items',
+            {
+                method: 'GET',
+            }
+        );
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    const newItem = { id: uuid(), title };
+        const data = await response.json();
+        console.log('new todo', response, data);
 
-    setItems([...items, newItem]);
-  };
+        setItems(data);
+    }, []);
 
-  return (
-    <div className="App">
-      <form>
-        <input value={title} onChange={newItemTitleChange} />
-        <button type="submit" onClick={submitHandler}>
-          Add
-        </button>
-      </form>
+    useEffect(() => {
+        getData();
+    }, []);
 
-      <ul>
-        {items.map((item) => {
-          return <li key={item.id}>{item.title}</li>;
-        })}
-      </ul>
-    </div>
-  );
+    const newItemTitleChange = ({ target: { value } }) => {
+        setTitle(value);
+    };
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+
+        const newItem = { id: uuid(), title };
+
+        await fetch(
+            'http://localhost:8001/api/todolist/add-new-task',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(newItem),
+            }
+        );
+
+        await getData();
+
+        setTitle('');
+    };
+
+    const deleteHandler = async (id) => {
+        await fetch(
+            'http://localhost:8001/api/todolist/delete-item',
+            {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ id }),
+            }
+        );
+      
+      await getData();
+    };
+
+    useEffect(() => {
+        console.log('Component was mounted');
+    }, []);
+
+    useEffect(() => {
+        console.log('Component was updated title', items);
+    }, [items]);
+
+    return (
+        <div className="App">
+            <form>
+                <input value={title} onChange={newItemTitleChange} />
+                <button type="submit" onClick={submitHandler}>
+                    Add
+                </button>
+            </form>
+
+            <ul>
+                {items.map((item) => {
+                    return (
+                        <li key={item.id}>
+                            {item.title}
+                            <button
+                                onClick={() => {
+                                    deleteHandler(item.id);
+                                }}
+                            >
+                                Delete
+                            </button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
