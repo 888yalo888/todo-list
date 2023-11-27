40ac438c00ed91dcf1be79cee80ac1a44c2ebae2
Olga <888yalosinger@ukr.net>
2023-11-27 12:22:08 -0500

    Remove back-edn

--- back-end/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-await mongoose.connect(
-    'mongodb+srv://olgaorlova241:druYgJ4o6vMYg4Q7@cluster0.e3sqktx.mongodb.net/todolist?retryWrites=true&w=majority'
-);
-// await mongoose.connect('mongodb://127.0.0.1:27017/todolist');
-
-export const Task = mongoose.model('Task', { title: String });
